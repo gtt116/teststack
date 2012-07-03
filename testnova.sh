1ef88df923c576778d9ad1799974362f3ebde1e7
hzgaott <hzgaott@corp.netease.com>
2012-07-03 16:18:23 +0800

    Add comment

--- testnova.sh
+++ testnova.sh
@@ -7,13 +7,21 @@ XTOKEN='x-auth-token:736b396227da44ec993593a28088b762'
 
 # http://114.113.199.9:8774/v1.1/149696bd1d1941afbeeb986b4e0fa75a/images/9496e46d-2a3f-4df0-9220-42151a8c2b1a
 
+
+# ---------------------------------
 echo '[+] get flavor'
 http -v get $URL/flavors $XTOKEN
 
+
+# ---------------------------------
 echo '[+] get servers'
 http -v get $URL/servers/detail $XTOKEN
 
+# ----------------------------------
 echo '[+] create instance: server-2'
+# Create a intance with same_host with bd976f4a-ddd7-407f-bd30-2b74428e3716,
+# and different host with bd976f4a-ddd7-407f-bd30-2b74428e3716, so that action
+# will be faild.
 http -v post $URL/servers $XTOKEN <<<  \
 '''
 {
