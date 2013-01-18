d03622aa3bf9a1b16db950f61ef06b88e1b78d6d
gtt116 <gtt116@126.com>
2013-01-17 22:34:07 -0800

    Create rpc_body.js
    rpc body example

---
+++ rpc_body.js
@@ -0,0 +1,26 @@
+# topic = 'compute.114-113-199-8'
+# msg = {'method': 'service_version',
+         'version': '2.0'}
+# rpc.call(admin_context, topic, msg)
+
+{
+    "_context_roles": [],
+    "_msg_id": "c263c6ab641e47b2b652511064306ddf",
+    "_context_quota_class": null,
+    "_context_request_id": "req-705a9196-bef3-4985-be63-3902931d6a00",
+    "_context_service_catalog": null,
+    "_context_auth_token": null,
+    "_context_unified_log_id": null,
+    "_context_user_id": null,
+    "_context_unified_log_seq": null,
+    "_context_project_name": null,
+    "_context_read_deleted": "no",
+    "_context_instance_lock_checked": false,
+    "_context_is_admin": true,
+    "version": "2.0",
+    "_context_project_id": null,
+    "_context_timestamp": "2013-01-18T06:28:36.742218",
+    "_context_user_name": null,
+    "method": "service_version",
+    "_context_remote_address": null
+}
