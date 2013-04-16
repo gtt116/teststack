8b10a06a92546e11da7fa572414350f038273ecf
TianTian <gtt116@126.com>
2013-04-16 17:26:12 +0800

    Make glance client example more refactable.

--- python-client/glance.py
+++ python-client/glance.py
@@ -1,7 +1,9 @@
 from glanceclient import Client
 
+
 version = '1'
-c = Client(version, endpoint='http://localhost:9292/',
-           token='ed04f011085345ee94725580da2149e9')
+token = '5ff6db3c3f534ee3af76edde26e8dcc0'
+endpoint = 'http://localhost:9292'
+c = Client(version, endpoint=endpoint, token=token)
 images = [i for i in c.images.list()]
 c.images.get('f3108ff6-60cc-452f-9f86-994ecf131cc8')
